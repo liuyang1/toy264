f697c973fd6a5dfeede22670dc640e9b39e6c4ea
liuyang1 <liuyang1@mail.ustc.edu.cn>
2016-07-30 22:14:54 +0800

    H261: GOB

--- h261/h261.py
+++ h261/h261.py
@@ -22,11 +22,33 @@ class Picture(GrammerUnit):
             self.prs(ge('PEI', 'u', 1))
             return self.PEI == 1
         self.prsseq(ges(ge('PSPARE', 'u', 8), until=fn, param=self))
-        self.GOB()
-
-    def GOB(self):
-        return
+        self.GOBs = [GOB(self.bsm)]
       
     def dump(self):
         print('Picture')
+        super().dump()
+        for gob in self.GOBs:
+            gob.dump()
+
+class GOB(GrammerUnit):
+    def prs(self, ge):
+        GrammerUnit._geparse(self, self.bsm, ge)
+
+    def prsseq(self, ges):
+        GrammerUnit._geparseseq(self, self.bsm, ges)
+
+    # warning: this param is BSM not BS
+    def __init__(self, bsm):
+        self.bsm = bsm
+        gelst = [ge('GBSC', 'u', 16),
+                 ge('GN', 'u', 4),
+                 ge('GQUANT', 'u', 5)]
+        self._geparselst(self.bsm, gelst)
+        def fn(self):
+            self.prs(ge('GEI', 'u', 1))
+            return self.GEI == 1
+        self.prsseq(ges(ge('GSPARE', 'u', 8), until=fn, param=self))
+      
+    def dump(self):
+        print('GOB')
         super().dump()
